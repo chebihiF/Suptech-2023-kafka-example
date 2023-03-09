263ac06c6f30031ba22f13568ecb10e9b9cf42fd
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-03-09 20:39:58 +0100

    initial commit

--- src/main/java/com/example/kafkaexample/config/KafkaTopicConfig.java
+++ src/main/java/com/example/kafkaexample/config/KafkaTopicConfig.java
@@ -1,2 +1,15 @@
-package com.example.kafkaexample.config;public class KafkaTopicConfig {
+package com.example.kafkaexample.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic topicConfig(){
+        return TopicBuilder.name("customer-data").build();
+    }
 }
